14c765e49092bf81b49d14a513821d1d5e6e819d
Christian Fleury <chfleurysiq@gmail.com>
2022-01-17 20:37:42 -0300

    test: make basic merge sort test

--- tests/unit/mergeSort.spec.js
+++ tests/unit/mergeSort.spec.js
@@ -1,10 +1,17 @@
-const mergeSort = require('../../src/functions/mergeSort');
-jest.mock('../../src/functions/mergeSort');
+const MergeSort = require('../../src/functions/mergeSort');
 
 describe('Merge Sort', () => {
-  test('should call mergeSort function with correct parameters', () => {
-    const params = [1.3, 4.3, 41.4, 5.154];
-    mergeSort(params);
-    expect(mergeSort).toHaveBeenCalledWith(params);
+  // test('should call mergeSort function with correct parameters', () => {
+  //   const params = [1.3, 4.3, 41.4, 5.154];
+  //   mergeSort(params);
+  //   expect(mergeSort).toHaveBeenCalledWith(params);
+  // });
+
+  test('should sucessfuly sort the array', () => {
+    let array = [1.3, 4.3, 41.4, 5.154];
+
+    MergeSort.sort(array);
+
+    expect(array).toEqual([1.3, 4.3, 5.154, 41.4]);
   });
 });
