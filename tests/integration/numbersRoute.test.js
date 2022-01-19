aa2fa585e87dec3a6979fe1bf35e8d18d699b098
Christian Fleury <chfleurysiq@gmail.com>
2022-01-19 01:54:14 -0300

    refactor: change test name

--- tests/integration/numbersRoute.test.js
+++ tests/integration/numbersRoute.test.js
@@ -1,12 +1,13 @@
 const request = require('supertest');
 const app = require('../../src/app');
+const etl = require('../../src/services/etl');
 
 describe('numbers route', () => {
   afterAll((done) => {
     done();
   });
 
-  test('should return a matching object at numbers route', async () => {
+  test('should return a matching object at numbers route succes', async () => {
     const response = await request(app).get('/numbers');
 
     expect(response.body).toMatchObject({
